0fd658bc5f417597df7886bc4689608ec0c85b38
da5atar <mass.sow@gmail.com>
2017-07-02 16:02:04 +0000

    Adds gulp browserSync task

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,8 @@ gulp.task('css', function () {
                   browsers: ["last 2 versions"]
                 })) // add autoprefixer just after the Sass conversion and before writing sourcemaps
                 .pipe(sourcemaps.write('./maps')) // writing of the sourcemaps comes after processing
-                .pipe(gulp.dest('dist/css'));
+                .pipe(gulp.dest('dist/css'))
+                .pipe(browserSync.stream());
 });
 
 /* 2. Images Optimization */
@@ -47,5 +48,23 @@ gulp.task('images', function(){
 /* 3. Create a copy task so that tasks that are not processed simply be copied to the dist/ */
 gulp.task('copy', function(){
   return gulp.src('src/*.html')
-                .pipe(gulp.dest('dist')); // if we just use gulp.dest() after gulp.src(), the result would be a copy of those files to the destination-folder
+                .pipe(gulp.dest('dist')) // if we just use gulp.dest() after gulp.src(), the result would be a copy of those files to the destination-folder
+                .pipe(browserSync.stream());
 });
+
+/* 4. Configure browser-sync and tell it to reload the browser everytime the HTML or CSS files are changed */
+
+gulp.task('browserSync', function(){
+  browserSync.init({ // initializes browserSync and tell it the root of the server (dist folder of the Website)
+    server: {
+      baseDir: 'dist'
+    }
+  })
+})
+
+/* to tell browserSync to update the page We need to add one pipe to the css task
+* and another to the copy task:.pipe(browserSync.stream())
+* It is important that it is the last pipe to run as browserSync will reload just after new or modified files are in place
+*/
+
+/* 5 Configure the watch task */
